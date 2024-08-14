d1cc2cb23703e7278b703849b0576abf7425f48e
12618 <1261801883@qq.com>
2024-08-14 23:15:23 +0800

    2024-08-14 1、netty客户端和服务端代码编写 2、服务端处理耗时业务的第一种方法 - 用户程序自定义的普通任务(存放在channel的eventLoop下的taskQueue队列中)

--- src/main/java/com/dd/netty/netty/simple/NettyServer.java
+++ src/main/java/com/dd/netty/netty/simple/NettyServer.java
@@ -17,33 +17,40 @@ public class NettyServer {
          * 1.创建两个线程组bossGroup 和 workerGroup
          * 2.bossGroup 只是处理连接请求， 真正的和客户端业务处理，会交给workerGroup完成
          * 3.两个都是无限循环
+         * 4.bossGroup 和 workerGroup 含有的子线程（NioEventLoop）个数 默认是 cpu核数 * 2
+         *      NettyRuntime.availableProcessors() * 2
          */
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
 
-        //创建服务器端的启动对象，配置参数
-        ServerBootstrap bootstrap = new ServerBootstrap();
+        try {
+            //创建服务器端的启动对象，配置参数
+            ServerBootstrap bootstrap = new ServerBootstrap();
 
-        //使用链式编程来进行设置
-        bootstrap.group(bossGroup, workerGroup) //设置两个线程组
-                .channel(NioServerSocketChannel.class) //使用NioServerSocketChannel 作为服务器的通道实现
-                .option(ChannelOption.SO_BACKLOG, 128) //设置线程队列等待连接个数
-                .childOption(ChannelOption.SO_KEEPALIVE, true) //设置保持活动连接状态
-                .childHandler(new ChannelInitializer<SocketChannel>() { //创建一个通道初始化对象（匿名对象）
-                    //给pipeline设置处理器
-                    @Override
-                    protected void initChannel(SocketChannel socketChannel) throws Exception {
-                        socketChannel.pipeline().addLast(null);
-                    }
-                }); //给我们的workerGroup 的 EventLoop 对应的管道设置处理器
+            //使用链式编程来进行设置
+            bootstrap.group(bossGroup, workerGroup) //设置两个线程组
+                    .channel(NioServerSocketChannel.class) //使用NioServerSocketChannel 作为服务器的通道实现
+                    .option(ChannelOption.SO_BACKLOG, 128) //设置线程队列等待连接个数
+                    .childOption(ChannelOption.SO_KEEPALIVE, true) //设置保持活动连接状态
+                    .childHandler(new ChannelInitializer<SocketChannel>() { //创建一个通道初始化对象（匿名对象）
+                        //给pipeline设置处理器
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new NettyServerHandler());
+                        }
+                    }); //给我们的workerGroup 的 EventLoop 对应的管道设置处理器
 
-        System.out.println("....服务器 is ready....");
+            System.out.println("....服务器 is ready....");
 
-        //绑定一个端口并且同步 生成了一个ChannelFuture对象
-        //启动服务器（并绑定端口）
-        ChannelFuture cf = bootstrap.bind(6668).sync();
+            //绑定一个端口并且同步 生成了一个ChannelFuture对象
+            //启动服务器（并绑定端口）
+            ChannelFuture cf = bootstrap.bind(6668).sync();
 
-        //对关闭通道进行监听
-        cf.channel().closeFuture().sync();
+            //对关闭通道进行监听
+            cf.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
 }
---
+++ src/main/java/com/dd/netty/netty/simple/NettyServerHandler.java
@@ -0,0 +1,80 @@
+package com.dd.netty.netty.simple;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * 说明
+ * 1.我们自定义一个Handler 需要继承netty 规定好的某个HandlerAdapter（规范）
+ * 2.这时我们自定义一个Handler， 才能称为一个handler
+ */
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+
+    //读取数据（这里我们可以读取客户端发送的消息）
+    /**
+     * @param ctx  ChannelHandlerContext ctx 上下文对象，含有管道pipeline 通道channel 地址
+     * @param msg  Object msg                就是客户端发送的数据 默认Object
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        /**
+         * 比如这里有一个非常耗时的业务 -> 异步执行 -> 提交该channel对应的 NIOEventLoop 的taskQueue中
+         */
+
+        //解决方案1  用户程序自定义的普通任务
+        ctx.channel().eventLoop().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(10 * 1000);
+                    ctx.writeAndFlush(Unpooled.copiedBuffer("hello,客户端~o(=•ェ•=)m222", CharsetUtil.UTF_8));
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        });
+        /**
+         * 这里再加入一个任务 等待20秒执行 实际执行效果是： 先等待上一个任务执行10秒结束后才开始执行当前任务 （因为只有一个主线程！！！）
+         * 其中taskQueue的size为2 表示有两个任务在任务队列中
+         */
+        ctx.channel().eventLoop().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(20 * 1000);
+                    ctx.writeAndFlush(Unpooled.copiedBuffer("hello,客户端~o(=•ェ•=)m333", CharsetUtil.UTF_8));
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        });
+
+        System.out.println("go on ........");
+
+        /*System.out.println("server ctx = " + ctx);
+        ByteBuf buf = (ByteBuf) msg;
+        System.out.println("客户端发送消息是：" + buf.toString(CharsetUtil.UTF_8));
+        System.out.println("客户端地址：" + ctx.channel().remoteAddress());*/
+    }
+
+    //读取数据完毕
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        /**
+         * writeAndFlush 是 write + flush
+         * 将数据写入到缓存，并刷新
+         * 一般讲， 我们对这个发送的数据进行编码
+         */
+        ctx.writeAndFlush(Unpooled.copiedBuffer("hello,客户端~o(=•ェ•=)m111", CharsetUtil.UTF_8));
+    }
+
+    //处理异常 一般是关闭通道
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
