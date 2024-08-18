a35182e2b8aa298f904aa09779922d4bd4924421
12618 <1261801883@qq.com>
2024-08-18 18:22:07 +0800

    2024-08-18 1、编写netty聊天室服务端的代码

---
+++ src/main/java/com/dd/netty/netty/groupchat/GroupChatServer.java
@@ -0,0 +1,59 @@
+package com.dd.netty.netty.groupchat;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class GroupChatServer {
+
+    private int port; //监听端口
+
+    public GroupChatServer(int port) {
+        this.port = port;
+    }
+
+    //编写run方法 处理客户端的请求
+    public void run() throws Exception {
+        //创建两个线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(); //默认为 cpu核数 * 2 = 24
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128) //设置等待队列的个数
+                    .childOption(ChannelOption.SO_KEEPALIVE, true) //保持连接
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            //获取到pipeline
+                            ChannelPipeline pipeline = socketChannel.pipeline();
+                            //向pipeline中加入解码器
+                            pipeline.addLast("", new StringDecoder());
+                            //向pipeline中加入编码器
+                            pipeline.addLast("", new StringEncoder());
+                            //加入自己的业务处理handler
+                            pipeline.addLast(new GroupChatServerHandler());
+                        }
+                    });
+            System.out.println("netty 服务器启动");
+            ChannelFuture channelFuture = bootstrap.bind(port).sync();
+            //监听关闭事件
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        new GroupChatServer(7000).run();
+    }
+}
