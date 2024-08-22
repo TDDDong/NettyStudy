2318659b618b5e2ffffa85a7b70463dfbafa1a93
12618 <1261801883@qq.com>
2024-08-22 21:05:26 +0800

    2024-08-22 1、模拟tcp粘包和拆包问题

---
+++ src/main/java/com/dd/netty/netty/tcp/MyServerInitializer.java
@@ -0,0 +1,14 @@
+package com.dd.netty.netty.tcp;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline pipeline = socketChannel.pipeline();
+
+        pipeline.addLast(new MyServerHandler());
+    }
+}
